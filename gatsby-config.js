53fd5046b56f6f009d92ce595da799d131cade7a
David Currie <david.alexander.currie@gmail.com>
2020-09-01 01:20:18 -0400

    update layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -83,7 +83,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
+              maxWidth: 800,
             },
           },
           {
