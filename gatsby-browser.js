0e86abb937286003598c089d02476262e00ca2df
David Currie <dac@10-18-232-213.dynapool.wireless.nyu.edu>
2020-09-16 09:20:39 -0400

    page refresh test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+// trigger an immediate page refresh when an update is found
+export const onServiceWorkerUpdateReady = () => window.location.reload()
