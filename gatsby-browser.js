cc8779cd58c3e626d19aa5f5a43b560dcf00b19f
David Currie <dac@10-18-232-213.dynapool.wireless.nyu.edu>
2020-09-16 09:32:15 -0400

    test 2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,4 +3,7 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 // trigger an immediate page refresh when an update is found
-export const onServiceWorkerUpdateReady = () => window.location.reload()
+
+export function onServiceWorkerUpdateReady() {
+  window.location.reload()
+}
