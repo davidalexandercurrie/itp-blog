06dd44f7d418cd24023ab9f31377ca7e1291122f
David Currie <davidalexandercurrie@gmail.com>
2021-10-06 03:02:48 -0400

    test

--- src/components/countingwords.js
+++ src/components/countingwords.js
@@ -3,19 +3,20 @@ import { io } from 'socket.io-client';
 const socket = io('https://word-counting-server.herokuapp.com/');
 
 const CountingWords = () => {
-  let [tweets, setTweets] = useState(null);
+  let [keys, setKeys] = useState(null);
+  let [counts, setCounts] = useState(null);
   useEffect(() => {
     socket.emit('msg', 'hello');
     socket.on('event', (data) => {
       console.log(data);
-      setTweets(data.data);
+      setKeys(data.keys);
+      setCounts(data.counts);
     });
   }, []);
   return (
     <>
       <div>hellllllo</div>
-      {/* {tweets && tweets.map((element) => <p>{element.text}</p>)} */}
-      {tweets && <div>tweets</div>}
+      {keys && keys.map((element) => <p>{`${element} ${counts[element]}`}</p>)}
     </>
   );
 };
